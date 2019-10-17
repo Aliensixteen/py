65b41c2bcf3ddfeb35c12cc8f3300765623eebcb
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-10-17 17:38:00 +0800

    Revert "Create 有问题的里程表.py"
    This reverts commit 5bb8f16a38d7b3a67842537efe80aab4c0656d6b.

--- 有问题的里程表.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-a,b = int(),int()
-a = num
-b = 0
-while b <= num:
-    b = b + 1
-    if b % 10 == 4 or (b // 10) % 10 == 4 or (b // 100) % 10 == 4:
-        a = a - 1
-print(a)
