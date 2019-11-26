9a2acbeb5cc378ce466cec2da2b081eb94446522
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-11-27 02:15:33 +0800

    Create 1120 最值交换.py

---
+++ 1120 最值交换.py
@@ -0,0 +1,29 @@
+n = int(input())
+n1 = [int(i1) for i1 in input().split()]
+a = 999999
+b = -999999 
+for i in n1:
+    if i > b:
+        b = i #最大判断
+for j in n1:
+    if a > j:
+        a = j #最小判断
+b1 = n1[-1] #最后一位
+a1 = n1[0] #第一位
+i2 = -1
+for k in n1:
+    i2 += 1
+    if k == a:
+        n1[0] = a
+        n1[i2] = a1 #最小换位
+        break
+b1 = n1[-1] #最后一位
+a1 = n1[0] #第一位
+i3 = -1
+for k1 in n1:
+    i3 += 1
+    if k1 == b:
+        n1[-1] = b
+        n1[i3] = b1 #最大换位
+        break
+print(*n1)
