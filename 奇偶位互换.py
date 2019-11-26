345901b159912546c94c19153e79277a97a3c042
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-11-27 02:27:40 +0800

    Delete 奇偶位互换.py

--- 奇偶位互换.py
+++
@@ -1,9 +0,0 @@
-C = int(input())
-for i in range(C):
-    n = input()
-    res = ''
-    for j in range(1,len(n),2):
-        res = res + n[j] + n[j-1]
-    if len(n) % 2 == 1:
-        res = res + n[-1]
-    print(res)
