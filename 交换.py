d9650a0d74632a47f037105012d7487ed770f200
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-11-27 02:33:45 +0800

    Delete 交换.py

--- 交换.py
+++
@@ -1,8 +0,0 @@
-import math
-n = input()
-res = ''
-for i in range(1,len(n)):
-    res = res + n[i] + n[i-1]
-    x = [int(j) for j in res]
-    x1 = x[0] * 10 + x[1]
-    print('%.2f'%(math.sqrt(x1)))
