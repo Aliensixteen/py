ab9759294b89343ddb19a3b475a9ee32fce6ea02
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-11-28 01:21:06 +0800

    Create 1100 求组合数（函数专题）.py

---
+++ 1100 求组合数（函数专题）.py
@@ -0,0 +1,14 @@
+#http://acm.zzuli.edu.cn/problem.php?id=1100
+def cj(x):
+    sum = 1
+    i = x
+    while i > 1:
+        sum *= i
+        i -= 1
+    return sum
+
+
+m , k = [int(i) for i in input().split()]
+num = int()
+num = cj(m) / (cj(k) * (cj(m-k)))
+print('%d'%num)
