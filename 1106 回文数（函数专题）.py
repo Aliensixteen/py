36e86dad40efd842955d31bea96c4417d3b7d783
Aliensixteen <zhuzhu+Aliensixteen@users.noreply.github.com>
2019-11-30 21:24:19 +0800

    Create 1106 回文数（函数专题）.py

---
+++ 1106 回文数（函数专题）.py
@@ -0,0 +1,11 @@
+def hw(x):
+    x1 = 0
+    while x != 0:
+        x1 = x1 * 10 + x % 10
+        x = x // 10
+    return x1
+
+m ,n = [int(i) for i in input().split()]
+for i1 in range(m,n):
+    if i1 == hw(i1):
+        print(i1,end=' ')
